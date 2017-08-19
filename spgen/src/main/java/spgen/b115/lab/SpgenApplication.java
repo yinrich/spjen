7591a8caad24bc7cf84d683b1a463875e4aee7fc
Richard Yin <yinrich@gmail.com>
2017-08-19 17:49:27 -0400

    Iniital check in

---
+++ spgen/src/main/java/spgen/b115/lab/SpgenApplication.java
@@ -0,0 +1,12 @@
+package spgen.b115.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgenApplication.class, args);
+	}
+}
---
+++ spgen/src/test/java/spgen/b115/lab/SpgenApplicationTests.java
@@ -0,0 +1,16 @@
+package spgen.b115.lab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpgenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
